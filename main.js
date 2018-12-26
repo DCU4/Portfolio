e87574320662a8138c7c33fee3e0bbb57ea077fd
DCU4 <DylanJConnor4@gmail.com>
2018-12-26 15:30:48 -0500

    adjust animation, layout styles

--- main.js
+++ main.js
@@ -16,9 +16,13 @@ links[i].addEventListener('click',function(){
 			
 			$('.about').animate({
 				opacity: '1'
-			});
+			}).css({
+				display: 'flex'
+				});
 			$('.work').animate({
 				opacity: '0'
+			}).css({
+				display: 'none'
 			});
 
 
@@ -27,16 +31,30 @@ links[i].addEventListener('click',function(){
 			aboutLink.classList.remove('selected');
 			workLink.classList.add('selected');
 
-			$('.about').animate({
-				opacity: '0'
-			});
 			$('.work').animate({
 				opacity: '1'
+			}).css({
+				display: 'flex'
+				});
+			$('.about').animate({
+				opacity: '0'
+			}).css({
+				display: 'none'
 			});
 
 		}
+
+		// if (about.style.opacity === '0') {
+		// 	about.style.display = 'none';
+		// 	console.log(about);
+		// } else {
+		// 	about.style.display = 'flex';
+		// 	console.log(about);
+		// }
 	
 });
 
 }
+
+
 
