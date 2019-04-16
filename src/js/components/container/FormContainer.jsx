6ca261d7ae44b8d8011c2c231693a425e52eb82d
Dylan Connor <dconnor@req.co>
2019-04-16 09:42:02 -0400

    begin setting up nav in react!

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Input from "../presentational/Input.jsx";
+import Nav from "../presentational/Nav.jsx";
 class FormContainer extends Component {
   constructor() {
     super();
     this.state = {
-      seo_title: ""
+      // seo_title: ""
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -15,16 +15,10 @@ class FormContainer extends Component {
   render() {
     const { seo_title } = this.state;
     return (
-      <form id="article-form">
-        <Input
-          text="SEO title"
-          label="seo_title"
-          type="text"
-          id="seo_title"
-          value={seo_title}
-          handleChange={this.handleChange}
-        />
-      </form>
+
+      <Nav />
+
+
     );
   }
 }
---
+++ src/js/components/presentational/Nav.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+// import FormContainer from "../container/FormContainer";
+
+export default class Nav extends Component {
+
+    render() {
+      const navList = [
+        {
+          title: 'Home',
+          link:'/home'
+        },
+        {
+          title:'About',
+          link:'/about'
+        }
+
+      ];
+      const navItems = navList.map((items) => <li className="menu-item"><a data-text="_______" className="link" id="work" href="#">{items.title}</a></li>)
+      return (
+        <nav>
+          <ul className="menu">
+            {navItems}
+          </ul>
+        </nav>
+      );
+    }
+  }
