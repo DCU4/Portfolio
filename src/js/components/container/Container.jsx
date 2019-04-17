18c8b483efb33860a5f11492354039fff08e86a4
Dylan Connor <dconnor@req.co>
2019-04-17 13:56:50 -0400

    Revert "Updates"
    This reverts commit 7ed013160079cb703c407f140a56958b9272053e.

---
+++ src/js/components/container/Container.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Nav from "../presentational/Nav.jsx";
+import About from "../presentational/About.jsx";
+import { Work } from "../presentational/Work.jsx";
+class Container extends Component {
+  constructor() {
+    super();
+    this.state = {
+      aboutShowing: false
+
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      aboutShowing: true
+      // change class name??
+    });
+    console.log(this.state);
+  }
+  render() {
+
+    return (
+      <div className="container">
+        <header>
+          <h1 className="title"><a href="/">Dylan Connor</a></h1>
+        </header>
+        <Nav
+          // onClick={this.handleChange}
+          className="link "
+          changeDisplay="test"
+
+        />
+        <About />
+        <main>
+          <Work
+            title='J.Walls'
+            link='https://dcu4.github.io/DCU4.github.io-J_Walls/'
+            img='../img/jwalls-gif.gif'
+          />
+          <Work
+            title='Esther Rivas'
+            link='https://dcu4.github.io/DCU4.github.io-Esther-Rivas/index.html'
+            img='../img/esther.gif'
+          />
+          <Work
+            title='Avnoe'
+            link='https://kee-app.herokuapp.com/login'
+            img='../img/drawing-upload-final.gif'
+          />
+        </main>
+
+
+
+
+      </div>
+
+
+
+    );
+  }
+}
+export default Container;
+
+const wrapper = document.getElementById("app");
+wrapper ? ReactDOM.render(<Container />, wrapper) : false;
