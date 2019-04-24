e3de72296749961778b2f41d03215e6272748c51
Dylan Connor <dconnor@req.co>
2019-04-24 17:44:13 -0400

    so that didnt work. trying something else

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
+  entry: './src/index.js',
   output: {
     path: __dirname + '/docs',
-    publicPath: '/img/'
+    // publicPath: '/img/'
   },
   module: {
     rules: [
