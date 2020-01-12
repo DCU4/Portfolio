50ad59ef548b70674d5bd43d889facf425119919
Dylan Connor <DylanJConnor4@gmail.com>
2020-01-11 20:52:48 -0500

    logic for mouse handling to change style of background grid!

--- src/js/components/container/Container.jsx
+++ src/js/components/container/Container.jsx
@@ -13,6 +13,7 @@ class Container extends Component {
       isAboutShowing: false,
       darkMode: false
     };
+    
   }
 
   handleShow() {
@@ -27,6 +28,70 @@ class Container extends Component {
     });
   }
 
+  handleMove = e => {
+    
+    // console.log(e.target.style);
+    let position = e.target.getBoundingClientRect();
+    // console.log(e.target.style);
+    e.target.style.transform = `skewY(${position.x}px)`;
+    // console.log(e.target.style.transform);
+    const container = document.querySelector('body');
+    const inner = document.querySelector(".grid");
+
+    // console.log(container);
+    // Mouse 
+    let mouse = {
+      _x: 0,
+      _y: 0,
+      x: 0,
+      y: 0,
+      updatePosition: function(event) {
+        let e = event || window.event;
+        this.x = e.clientX - this._x;
+        this.y = (e.clientY - this._y) * -1;
+      },
+      setOrigin: function(e) {
+        this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
+        this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
+      },
+      show: function() { return '(' + this.x + ', ' + this.y + ')'; }
+    }
+
+    mouse.setOrigin(container);
+    
+
+    let counter = 0;
+    let updateRate = 10;
+    let isTimeToUpdate = function() {
+      return counter++ % updateRate === 0;
+    };
+
+
+    let update = function(event) {
+      mouse.updatePosition(event);
+      console.log(mouse,inner.offsetHeight);
+      updateTransformStyle(
+        (mouse.y / inner.offsetHeight/2).toFixed(2),
+        (mouse.x / inner.offsetWidth/2).toFixed(2)
+      );
+    };
+    
+    let updateTransformStyle = function(x, y) {
+      // let style = "skewX(" + ( y*50) + "deg) skewY(" +x*25  + "deg)";
+      let style = `translate(${y*250}px, ${x*250}px ) skew(${y*50}deg, ${x*25}deg)`;
+      // let style = `rotate(${x/y}deg)`;
+      inner.style.transform = style;
+      inner.style.webkitTransform = style;
+      inner.style.mozTransform = style;
+      inner.style.msTransform = style;
+      inner.style.oTransform = style;
+    };
+    if (isTimeToUpdate()) {
+      update(e);
+    }
+
+  }
+
 
 
 
@@ -36,9 +101,12 @@ class Container extends Component {
     const isAboutShowing = this.state.isAboutShowing;
     const darkMode = this.state.darkMode;
     darkMode ? document.body.classList = 'dark-mode' : document.body.classList =  '';
+    
+
+    
 
     return (
-      <div className={darkMode ? 'container dark-mode' : 'container'}>
+      <div  onMouseMove={this.handleMove}  className={darkMode ? 'container dark-mode' : 'container'}>
 
         <header>
           <h1 className="title">
@@ -51,15 +119,15 @@ class Container extends Component {
         
         {isAboutShowing ? (
           <main className="aboutShowing">
-             <div className={darkMode ? 'grid dark-mode' : 'grid'}>
-          <span className="grid-item"></span>
-          <span className="grid-item"></span>
-          <span className="grid-item"></span>
-          <span className="grid-item"></span>
-          <span className="grid-item"></span>
-          <span className="grid-item"></span>
+             <div  className={darkMode ? 'grid dark-mode' : 'grid'}>
+              <span className="grid-item"></span>
+              <span className="grid-item"></span>
+              <span className="grid-item"></span>
+              <span className="grid-item"></span>
+              <span className="grid-item"></span>
+              <span className="grid-item"></span>
 
-        </div>
+            </div>
             <About
               darkMode={darkMode ? 'dark-mode' : ''}
             />
@@ -68,6 +136,12 @@ class Container extends Component {
         ) : (
           <main className="workShowing">
              <div className={darkMode ? 'grid dark-mode' : 'grid'}>
+          <span className="grid-item"></span>
+          <span className="grid-item"></span>
+          <span className="grid-item"></span>
+          <span className="grid-item"></span>
+          <span className="grid-item"></span>
+          <span className="grid-item"></span>
           <span className="grid-item"></span>
           <span className="grid-item"></span>
           <span className="grid-item"></span>
