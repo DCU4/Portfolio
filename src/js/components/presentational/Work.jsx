a0d766f6e32e36d26d5b4b6f5eb8a1a72f241aeb
Dylan Connor <DylanJConnor4@gmail.com>
2021-03-10 22:20:32 -0500

    cleanup; alt tag

--- src/js/components/presentational/Work.jsx
+++ src/js/components/presentational/Work.jsx
@@ -7,10 +7,12 @@ export const Work = (props) => {
 
             <div className="work info">
                 <section>
-                    <a target="_blank" className={props.darkMode} data-text="__________________" href={props.link}>{props.title}</a>
+                    <a target="_blank" className={props.darkMode}  href={props.link}>{props.title}</a>
                     <p>{props.info}</p>
                     <div className="project">
-                        <a target="_blank" href={props.link}><img src={props.img} /></a>
+                        <a target="_blank" href={props.link}>
+                          <img src={props.img} alt={props.title} />
+                        </a>
                     </div>
                 </section>
             </div>
